cf97f4c45fc3e5a515ad3207a5577c6005839596
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-01-22 22:51:14 +0100

    example solution

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let konecnyText = ""
+function Zasifruj (text: string, posun: number) {
+    for (let i = 0; i <= text.length - 1; i++) {
+        konecnyText = "" + konecnyText + Sifra.Zasifruj(text.charAt(i), posun)
+    }
+    return konecnyText
+}
+function Desifruj (text: string, posun: number) {
+    for (let j = 0; j <= text.length - 1; j++) {
+        konecnyText = "" + konecnyText + Sifra.Desifruj(text.charAt(j), posun)
+    }
+    return konecnyText
+}
 basic.forever(function () {
-    basic.showString(Sifra.Desifruj("a", -1))
+    basic.showString("" + (Desifruj("hovq zclal", 7)))
 })
