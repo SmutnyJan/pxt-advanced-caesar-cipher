fc51d6e83f3bbaa8244ccedbca4284a26ef7f570
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-02 21:21:46 +0100

    another one

--- cipher.ts
+++ cipher.ts
@@ -7,7 +7,7 @@
 /**
  * Custom blocks
  */
-//% weight=100 color=#ff6600 icon="\uf023"
+//% weight=100 color=#ff6601 icon="\uf023"
 namespace Sifra {
     const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     /**
