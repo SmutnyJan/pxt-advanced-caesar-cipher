d5d272221a698646018215b3d6272904c5efa56d
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-03-13 22:07:22 +0100

    Aktualizovat cipher.ts

--- cipher.ts
+++ cipher.ts
@@ -9,7 +9,7 @@ namespace Sifra {
      */
     //% block="Zašifruj znak %znak caesarovou šifrou s posunem %posun"
     export function zasifrujZnak(znak: string, posun: number): string {
-        znak = znak.toLowerCase()[0];
+        znak = checkCharForDiacritics(znak.toLowerCase()[0])
         let indexZnaku = znaky.indexOf(znak);
         if (indexZnaku != -1) {
             let finalniIndex = indexZnaku + posun;
@@ -33,7 +33,7 @@ namespace Sifra {
      */
     //% block="Dešifruj znak %znak caesarovou šifrou s posunem %posun"
     export function desifrujZnak(znak: string, posun: number): string {
-        znak = znak.toLowerCase()[0];
+        znak = checkCharForDiacritics(znak.toLowerCase()[0])
         let indexZnaku = znaky.indexOf(znak);
         if (indexZnaku != -1) {
             let finalniIndex = indexZnaku - posun;
@@ -48,6 +48,113 @@ namespace Sifra {
             return znak
         }
 
+    }
+
+    function checkCharForDiacritics(char: string): string {
+        let aChars = ["á", "å", "ä", "â", "ą", "ā", "à", "ã", "â", "ă", "ả", "ạ"]
+        let cChars = ["č", "ĉ", "ç", "ć"]
+        let dChars = ["ď", "đ"]
+        let eChars = ["é", "ě", "è", "ê", "ë", "ę", "ė", "ē"]
+        let gChars = ["ĝ", "ģ", "ǧ"]
+        let hChars = ["ĥ", "ȟ"]
+        let iChars = ["í", "î", "ï"]
+        let kChars = ["ķ"]
+        let lChars = ["ł", "ĺ", "ľ"]
+        let nChars = ["ň", "ņ", "ń", "ñ"]
+        let oChars = ["ó", "ø", "õ", "ö", "ô", "ő", "ơ"]
+        let rChars = ["ř", "ŕ"]
+        let sChars = ["š", " ś", "ș"]
+        let tChars = ["ť", "ț"]
+        let uChars = ["ú", "ů", "ŭ", "ü", "ù", "û", "ų", "ū", "ű", "û", "ư"]
+        let yChars = ["ý", "ÿ"]
+        let zChars = ["ž", "ź", "ż"]
+
+        for (const element of aChars) {
+            if (element == char) {
+                return "a"
+            }
+        }
+        for (const element of cChars) {
+            if (element == char) {
+                return "c"
+            }
+        }
+        for (const element of dChars) {
+            if (element == char) {
+                return "d"
+            }
+        }
+        for (const element of eChars) {
+            if (element == char) {
+                return "e"
+            }
+        }
+        for (const element of gChars) {
+            if (element == char) {
+                return "g"
+            }
+        }
+        for (const element of hChars) {
+            if (element == char) {
+                return "h"
+            }
+        }
+        for (const element of iChars) {
+            if (element == char) {
+                return "i"
+            }
+        }
+        for (const element of kChars) {
+            if (element == char) {
+                return "k"
+            }
+        }
+        for (const element of lChars) {
+            if (element == char) {
+                return "l"
+            }
+        }
+        for (const element of nChars) {
+            if (element == char) {
+                return "n"
+            }
+        }
+        for (const element of oChars) {
+            if (element == char) {
+                return "o"
+            }
+        }
+        for (const element of rChars) {
+            if (element == char) {
+                return "r"
+            }
+        }
+        for (const element of sChars) {
+            if (element == char) {
+                return "s"
+            }
+        }
+        for (const element of tChars) {
+            if (element == char) {
+                return "t"
+            }
+        }
+        for (const element of uChars) {
+            if (element == char) {
+                return "u"
+            }
+        }
+        for (const element of yChars) {
+            if (element == char) {
+                return "y"
+            }
+        }
+        for (const element of zChars) {
+            if (element == char) {
+                return "z"
+            }
+        }
+        return char
     }
 
 }
